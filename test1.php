ed5506c41a8c36462ac67239b96bfa9e6d46524a
Sanjeev Shrestha <sanjeevshrestha2004@gmail.com>
2014-09-03 00:02:38 +0545

    Update test1.php

--- test1.php
+++ test1.php
@@ -1,9 +1,9 @@
 <?php
 
 $config['database']['host']='localhost';
-$config['database']['username']='root';
-$config['database']['password']='8epecAfe';
-$config['database']['database']='byylive_db';
+$config['database']['username']='xxxxx';
+$config['database']['password']='xxxxx';
+$config['database']['database']='xxxxx';
 
 $conn=new mysqli($config['database']['host'],$config['database']['username'],$config['database']['password'],$config['database']['database']);
 
